54f8807a834b5d8565bec4981130b4e0925dbb18
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-05-10 18:19:22 +0800

    Update diy-immortalwrt.sh

--- diy/diy-immortalwrt.sh
+++ diy/diy-immortalwrt.sh
@@ -71,6 +71,16 @@ rm -rf feeds/luci/applications/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr
 rm -rf feeds/luci/packages/net/{shadowsocksr-libev-ssr-check,shadowsocksr-libev-ssr-local,shadowsocksr-libev-ssr-redir,shadowsocksr-libev-ssr-server}
 # 将packages源的相关文件替换成passwall_packages源的
 rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-passwall
+
+rm -rf feeds/third_party
+rm -rf feeds/istoreos_ipk/op-mosdns
+rm -rf feeds/istoreos_ipk/xray-core
+rm -rf feeds/istoreos_ipk/sing-box
+rm -rf feeds/istoreos_ipk/chinadns-ng
+rm -rf feeds/istoreos_ipk/microsocks
+rm -rf feeds/istoreos_ipk/dns2socks
+rm -rf feeds/istoreos_ipk/ipt2socks
+
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
@@ -87,14 +97,17 @@ cp -r feeds/small/v2ray-geoip feeds/packages/net
 cp -r feeds/small/sing-box feeds/packages/net
 cp -r feeds/small/chinadns-ng feeds/packages/net
 cp -r feeds/small/dns2socks feeds/packages/net
-cp -r feeds/small/dns2tcp feeds/packages/net
+cp -r feeds/small/ipt2socks feeds/packages/net
 cp -r feeds/small/microsocks feeds/packages/net
+
 
 # istoreos-theme
 rm -rf feeds/istoreos_ipk/theme/luci-theme-argon
 cp -r feeds/theme/luci-theme-argon feeds/luci/themes/luci-theme-argon
 
 rm -rf feeds/third/luci-app-argon-config
+rm -rf feeds/istoreos_ipk/luci-app-argon-config
+
 cp -r feeds/theme/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
 
 # Git稀疏克隆，只克隆指定目录到本地
