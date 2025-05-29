53b19f727c570659cd2194593a3814eeabdac4ff
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-05-29 22:34:53 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -155,7 +155,9 @@ function merge_package() {
 
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-app-argon-config
-
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-passwall
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
 
 
 # golong1.24.2依赖
