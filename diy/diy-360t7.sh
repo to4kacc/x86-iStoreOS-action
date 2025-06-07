8413adfa8851b90fc71b505f002a367d618d5018
Jaykwok2999 <61024435@qq.com>
2025-06-07 19:15:30 +0800

    Update diy-360t7.sh

--- diy/diy-360t7.sh
+++ diy/diy-360t7.sh
@@ -62,6 +62,7 @@ rm -rf feeds/istoreos_ipk/patch/istoreos-files
 rm -rf feeds/istoreos_ipk/vlmcsd
 rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-vlmcsd
 rm -rf package/diy/luci-app-ota
+rm -rf feeds/istoreos_ipk/naiveproxy
 
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
