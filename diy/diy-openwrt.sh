e559effd57c62063a8688070a9050154d343b5c8
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-06-27 06:41:01 +0800

    Update diy-openwrt.sh

--- diy/diy-openwrt.sh
+++ diy/diy-openwrt.sh
@@ -164,10 +164,12 @@ git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-ap
 git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
 git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
 
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
+rm -rf feeds/kenzo/luci-app-dockerman
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dockerman
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-upnp
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 
