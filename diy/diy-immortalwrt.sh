8393075992b05f9103328c3dbe0bf22b172a6af3
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-06-29 10:13:28 +0800

    Update diy-immortalwrt.sh

--- diy/diy-immortalwrt.sh
+++ diy/diy-immortalwrt.sh
@@ -163,11 +163,10 @@ git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-th
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-app-argon-config
 git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
 git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
-
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-mosdns
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages mosdns
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-tailscale
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages tailscale
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-upnp
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 
