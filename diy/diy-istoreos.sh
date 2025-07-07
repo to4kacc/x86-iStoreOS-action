753c44a4b9d65c97ea794c08b9f541ec884ddbda
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-07 12:47:09 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -157,9 +157,9 @@ function merge_package() {
     done
     cd "$rootdir"
 }
-#rm -rf package/diy/luci-app-ota
-#git_sparse_clone main https://github.com/sos801107/istoreos-ota luci-app-ota
-#git_sparse_clone main https://github.com/sos801107/github-ota fw_download_tool
+rm -rf package/diy/luci-app-ota
+git_sparse_clone main https://github.com/sos801107/istoreos-ota luci-app-ota
+git_sparse_clone main https://github.com/sos801107/istoreos-ota fw_download_tool
 
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-theme-argon
 git_sparse_clone openwrt-24.10 https://github.com/sbwml/luci-theme-argon luci-app-argon-config
