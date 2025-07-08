610acad7ec3c1cf06f739d03d51c7865e7e96914
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-07-08 09:24:16 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p files/etc/config
-wget -qO- https://raw.githubusercontent.com/sos801107/TL-XDR608X/refs/heads/main/etc/openclash > files/etc/config/openclash
-#wget -qO- https://raw.githubusercontent.com/liandu2024/clash/refs/heads/main/main_router/openclash > files/etc/config/openclash
+#wget -qO- https://raw.githubusercontent.com/sos801107/TL-XDR608X/refs/heads/main/etc/openclash > files/etc/config/openclash
+wget -qO- https://raw.githubusercontent.com/liandu2024/clash/refs/heads/main/main_router/openclash > files/etc/config/openclash
 wget -qO- https://raw.githubusercontent.com/sos801107/TL-XDR608X/refs/heads/main/etc/mosdns > files/etc/config/mosdns
 wget -qO- https://raw.githubusercontent.com/sos801107/TL-XDR608X/refs/heads/main/etc/smartdns > files/etc/config/smartdns
 
