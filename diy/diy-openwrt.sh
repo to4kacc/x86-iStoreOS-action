9fc7030fc6acff6599233c180a9cfe612d38bfbb
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-08-02 20:20:18 +0800

    Update diy-openwrt.sh

--- diy/diy-openwrt.sh
+++ diy/diy-openwrt.sh
@@ -179,7 +179,7 @@ rm -rf feeds/small/luci-app-openclash
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
 
 rm -rf package/feeds/packages/rust
-git_sparse_clone openwrt-24.10 https://github.com/immortalwrt/packages/lang rust
+git_sparse_clone openwrt-24.10 https://github.com/immortalwrt/packages lang/rust
 
 # 更改时间戳
 rm -rf scripts/get_source_date_epoch.sh
