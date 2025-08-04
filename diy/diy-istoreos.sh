0668eec6ded51346e67afb6ac0014ecf1f513868
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-08-04 11:33:34 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -101,7 +101,7 @@ cp -r feeds/small/xray-core feeds/packages/net
 cp -r feeds/small/mosdns feeds/packages/net
 cp -r feeds/small/v2ray-geodata feeds/packages/net
 cp -r feeds/small/v2ray-geoip feeds/packages/net
-cp -r feeds/small/sing-box feeds/packages/net
+#cp -r feeds/small/sing-box feeds/packages/net
 cp -r feeds/small/chinadns-ng feeds/packages/net
 cp -r feeds/small/dns2socks feeds/packages/net
 cp -r feeds/small/ipt2socks feeds/packages/net
@@ -180,7 +180,8 @@ git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 rm -rf feeds/small/luci-app-openclash
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
 
-
+rm -rf feeds/small/sing-box
+git_sparse_clone main https://github.com/sos801107/packages sing-box
 
 
 # 更改时间戳
