9601f34174c3861e72029add2de66d49fa0ff6f9
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-11-10 09:48:35 +0800

    Update diy-istoreos-rootfs.sh

--- diy/diy-istoreos-rootfs.sh
+++ diy/diy-istoreos-rootfs.sh
@@ -53,6 +53,7 @@ rm -rf feeds/third/{luci-app-argon-config,luci-theme-argon}
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf packages/istoreos-files
+rm -rf package/feeds/small/dns2socks
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
